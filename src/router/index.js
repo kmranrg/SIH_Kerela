1abcc3e38f1a4edea5c8813d92bca15bd61bd21d
Rajdeep Sharma <rajdeep.sharma17@gmail.com>
2018-03-07 02:33:51 +0530

    added comparing options

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AppCompare from '@/components/AppCompare'
+import AppHome from '@/components/AppHome'
+
 
 Vue.use(Router)
 
@@ -8,8 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'AppHome',
+      component: AppHome
+    },
+    {
+      path: '/compare',
+      name: 'AppCompare',
+      component: AppCompare
     }
   ]
 })
