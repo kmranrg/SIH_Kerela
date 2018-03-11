64dc585785ddfa2a50ce9f4b9e2c93ea731f06be
Rajdeep Sharma <rajdeep.sharma17@gmail.com>
2018-03-11 16:30:22 +0530

    redesigned hospital-profile

--- src/components/HospitalProfile.vue
+++ src/components/HospitalProfile.vue
@@ -13,12 +13,35 @@
         <v-avatar
           :tile="tile"
           :size="175"
-          class="grey lighten-4"
+          class="grey lighten-4 ml-5 "
         >
-          <img src="https://vuetifyjs.com/static/apple-touch-icon-180x180.png" alt="avatar">
+          <img src="@/assets/hospital.svg" alt="avatar">
         </v-avatar>
       </v-flex>
+      <div class="bt mb-5 mr-3">
+         <div class="text-xs-center">
+          <v-btn round color="orange darken-1" dark><v-icon left >book</v-icon>Book Appointment</v-btn>
+         </div>
+         <div class="text-xs-center">
+          <v-btn round color="purple accent-4" dark><v-icon left >comment</v-icon>Submit Review</v-btn>
+         </div>
+         <div class="text-xs-center">
+           
+          <v-btn round color="red accent-3" dark><v-icon left >favorite</v-icon>Favorite</v-btn>
+         </div>      
+      </div>
     </div>
-    <mobile-footer />
+
   </div>
 </template>
+
+<style>
+.bt {
+  display: flex;
+  flex-direction:row;
+  flex-wrap: wrap;
+  justify-content: flex-end;
+
+}
+</style>
+
